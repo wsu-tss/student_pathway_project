d36b6051a643bf3c05b527c7a5aeac7f336120f7
Pushkar Kadam <pushkarkadam17@outlook.com>
2020-08-12 10:15:34 +1000

    Saving and show weights on edges flexibility added as per #36.

--- adjacency_method/network_graph.py
+++ adjacency_method/network_graph.py
@@ -6,7 +6,7 @@ from datetime import datetime
 import sys
 import os
 
-def network_graph(matrix, units, figure_size=(50,50), color_node="red", color_edges="Blues", resolution=500, file_location="images/network_results/", label_position=0.3, edge_width=2, layout_type="circular", node_labels=True):
+def network_graph(matrix, units, figure_size=(50,50), color_node="red", color_edges="Blues", resolution=500, file_location="images/network_results/", label_position=0.3, edge_width=2, layout_type="circular", node_labels=True, save_figure=True, show_weights=True, edge_radius=0.1, size_node=300):
     """Saves and displays the network Diagram
 
     Keyword arguments:
@@ -14,13 +14,17 @@ def network_graph(matrix, units, figure_size=(50,50), color_node="red", color_ed
     units -- list of units present in the dataset
     figure_size -- a tuple that determines the size of matplotlib diagram. Default value is (50,50)
     color_node -- color of nodes. Default value is "red"
-    color_edges -- cmap color used in matplotlib used for different shades of color
-    resolution -- image resolution used in saving the diagram. Default value is 500.
+    color_edges -- cmap color used in matplotlib used for different shades of color. Default is "Blues"
+    resolution -- image resolution used in saving the diagram. Default value is 500
     file_location -- location to store the image. Default value is "images/test_dataset"
     label_position -- Adjust the position of the labels on the edges. Default value is 0.3
-    edge_width -- Size of the width of the edge. Default value is 2.
+    edge_width -- Size of the width of the edge. Default value is 2
     layout_type -- type of layout for the graph. Default is "circular"
     node_labels -- Boolean to show/hide the node labels. Default is True
+    save_figure -- Boolean to save figure. Default is True
+    show_weights -- Boolean to show weights on the network diagram. Default is True
+    edge_radius -- Float value of the radius of the edge. Default is 0.1
+    size_node -- Size of nodes. Default is 300
 
     Returns:
     None
@@ -67,21 +71,35 @@ def network_graph(matrix, units, figure_size=(50,50), color_node="red", color_ed
     else:
         position = nx.spring_layout(G)
 
-    # Draw the graph with color codes
-    nx.draw(G, pos=position, with_labels=node_labels, connectionstyle='arc3, rad=0.1', edge_list=edges, node_color=color_node, edge_color=weights, width=2, edge_cmap=plt.get_cmap(color_edges))
+    try:
+        # Draw the graph with color codes
+        nx.draw(G, pos=position, with_labels=node_labels, connectionstyle='arc3, rad={}'.format(edge_radius), edge_list=edges, node_color=color_node, node_size=size_node, edge_color=weights, width=edge_width, edge_cmap=plt.get_cmap(color_edges))
+    except ValueError as e:
+        print(e)
+        # Uses default values to display Graph
+        nx.draw(G, pos=position, with_labels=node_labels, connectionstyle='arc3, rad=0.1', edge_list=edges, edge_color=weights, width=2, edge_cmap=plt.cm.Blues)
 
-    # Draw the graph with edge labels
-    nx.draw_networkx_edge_labels(G, pos=position, label_pos=label_position, connectionstyle='arc3, rad=0.1')
+    # Checks if the user requested the weigts on graph
+    if show_weights == True:
+        try:
+            # Draw the graph with edge labels
+            nx.draw_networkx_edge_labels(G, pos=position, label_pos=label_position, connectionstyle='arc3, rad={}'.format(edge_radius))
+        except ValueError as e:
+            print(e)
+            # Uses default values to display graph
+            nx.draw_networkx_edge_labels(G, pos=position, label_pos=label_position, connectionstyle='arc3, rad=0.1')
 
-    # File location using timestamp
-    file_name = file_location + str(int(datetime.timestamp(datetime.now()))) + ".png"
+    # Saves the network diagram in a file file location
+    if save_figure == True:
+        # File location using timestamp
+        file_name = file_location + str(int(datetime.timestamp(datetime.now()))) + ".png"
 
-    # Checking if the folder does not exist
-    if not os.path.exists(file_location):
-        os.makedirs(file_location)
+        # Checking if the folder does not exist
+        if not os.path.exists(file_location):
+            os.makedirs(file_location)
 
-    # Saves the file with the filename as timestamp
-    plt.savefig(file_name, format="PNG", dpi=resolution)
+        # Saves the file with the filename as timestamp
+        plt.savefig(file_name, format="PNG", dpi=resolution)
 
     plt.show()
 
